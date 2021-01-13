ca8b776c5a565cae596f4a2e2f2a280ee2112bdf
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-13 12:02:45 -0500

    commit tree a76f2e529dce6cc0b36366c779d8691a6bd4e90a

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(obj){
+  let result = obj.find(ele => ele.result === "W")
+
+  if (result) {
+    return result.year
+  } else {
+    return undefined
+  }
+  
+}
+
+
+// ele.year === winYear
